86a33adfd04d558efbce7ba02ad55d20421f3f5e
KokiOta <ma20015@shibaura-it.ac.jp>
2021-03-15 23:52:24 +0900

    adxl345 was implemented

--- sensor/adxl345.js
+++ sensor/adxl345.js
@@ -1,16 +1,46 @@
+const ADXL345 = require('adxl345-sensor');
+const adxl345 = new ADXL345();
+
+const getAcceleration = () => {
+	adxl345.getAcceleration(true) // true for g-force units, else false for m/s²
+		.then((acceleration) => {
+			console.log(`acceleration = ${JSON.stringify(acceleration, null, 2)}`);
+			setTimeout(getAcceleration, 1000);
+		})
+		.catch((err) => {
+			console.log(`ADXL345 read error: ${err}`);
+			setTimeout(getAcceleration, 2000);
+		});
+};
+
+// Initialize the ADXL345 accelerometer
+//
+adxl345.init()
+  .then(() => {
+    console.log('ADXL345 initialization succeeded');
+    getAcceleration();
+  })
+  .catch((err) => console.error(`ADXL345 initialization failed: ${err} `));
+
 module.exports = function(red) {
-	function adxl345(conf) {
+	function Adxl345(conf) {
 		red.nodes.createNode(this, conf);
 		var node = this;
 
 		try {
 			node.on('input', function(msg) {
-				msg.payload = "Hello!";
-				node.send(msg);
+				adxl345.init()
+					.then(() => {
+						adxl345.getAcceleration(false)
+							.then((accel) => {
+								msg.payload = accel;
+								node.send(msg);
+							})
+					})
 			});
 		}catch(e) {
 			node.error(e);
 		}
 	}
-	red.nodes.registerType("adxl345", adxl345);
+	red.nodes.registerType("adxl345", Adxl345);
 }
