0702c37a7f52e6a65c514a83501a37a50b40c871
KokiOta <ma20015@shibaura-it.ac.jp>
2021-03-18 20:18:27 +0900

    bme680 was implemented

--- sensor/bme680.js
+++ sensor/bme680.js
@@ -1,15 +1,23 @@
+const { Bme680 } = require('bme680-sensor');
+const bme680 = new Bme680(1, 0x77);
+
 module.exports = function(red) {
-	function bme680(conf) {
+	function getEnvironment(conf) {
 		red.nodes.createNode(this, conf);
 		var node = this;
 
 		try {
 			node.on('input', function(msg) {
-				node.send("Hello!");
+				bme680.initialize()
+					.then(async () => {
+						var obj = await bme680.getSensorData();
+						msg.payload = obj.data;
+						node.send(msg);
+					})
 			});
 		}catch(e) {
 			node.error(e);
 		}
 	}
-	red.nodes.registerType("bme680", bme680);
+	red.nodes.registerType("bme680", getEnvironment);
 }
