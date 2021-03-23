4703b04455c4964ba2e03776818d7478cc561fca
KokiOta <ma20015@shibaura-it.ac.jp>
2021-03-24 04:21:15 +0900

    rgbled was implemented

--- actuator/rgbled.js
+++ actuator/rgbled.js
@@ -1,3 +1,8 @@
+const Gpio = require('pigpio').Gpio;
+const Red = new Gpio(17, {mode: Gpio.OUTPUT});
+const Green = new Gpio(27, {mode: Gpio.OUTPUT});
+const Blue = new Gpio(22, {mode: Gpio.OUTPUT});
+
 module.exports = function(red) {
 	function rgbled(conf) {
 		red.nodes.createNode(this, conf);
@@ -5,7 +10,13 @@ module.exports = function(red) {
 
 		try {
 			node.on('input', function(msg) {
-				node.send("Hello!");
+				if(0 <= msg.red && msg.red <= 255)
+					Red.pwmWrite(255 - msg.red);
+				if(0 <= msg.green && msg.green <= 255)
+					Green.pwmWrite(255 - msg.green);
+				if(0 <= msg.blue && msg.blue <= 255)
+					Blue.pwmWrite(255 - msg.blue);
+				node.send(msg);
 			});
 		}catch(e) {
 			node.error(e);
