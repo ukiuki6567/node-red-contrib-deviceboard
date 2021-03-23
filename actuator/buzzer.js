2ccdc534313d2eda1816099863d408c2e90d2754
KokiOta <ma20015@shibaura-it.ac.jp>
2021-03-24 03:15:33 +0900

    buzzer was  implement

--- actuator/buzzer.js
+++ actuator/buzzer.js
@@ -1,15 +1,27 @@
+const Gpio = require('pigpio').Gpio;
+const buzzer =  new Gpio(18, {
+	mode: Gpio.OUTPUT
+});
+
+function msleep(n){
+	Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
+}
+
 module.exports = function(red) {
-	function buzzer(conf) {
+	function Buzzer(conf) {
 		red.nodes.createNode(this, conf);
 		var node = this;
 
 		try {
 			node.on('input', function(msg) {
-				node.send("Hello!");
+				buzzer.hardwarePwmWrite(msg.frequency, 500000);
+				msleep(msg.miliseconds);
+				buzzer.pwmWrite(0);
+				node.send(msg);
 			});
 		}catch(e) {
 			node.error(e);
 		}
 	}
-	red.nodes.registerType("buzzer", buzzer);
+	red.nodes.registerType("buzzer", Buzzer);
 }
