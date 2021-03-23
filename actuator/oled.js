950a4a0559f28bb819a33a382a8a0a00b8cc464b
KokiOta <ma20015@shibaura-it.ac.jp>
2021-03-24 03:41:59 +0900

    oled was implemented

--- actuator/oled.js
+++ actuator/oled.js
@@ -1,15 +1,38 @@
+var bus = 1;
+
+var i2c = require('i2c-bus'),
+    i2cBus = i2c.openSync(bus),
+    oled = require('oled-i2c-bus');
+
+const SIZE_X=128,
+      SIZE_Y=64;
+
+var opts = {
+  width: SIZE_X,
+  height: SIZE_Y,
+  address: 0x3C
+};
+
+var oled = new oled(i2cBus, opts);
+oled.clearDisplay();
+oled.turnOnDisplay();
+
+var font = require('oled-font-5x7');
+
 module.exports = function(red) {
-	function oled(conf) {
+	function Oled(conf) {
 		red.nodes.createNode(this, conf);
 		var node = this;
 
 		try {
 			node.on('input', function(msg) {
-				node.send("Hello!");
+				oled.clearDisplay();
+				oled.setCursor(1, 1);
+				oled.writeString(font, msg.size, msg.payload, 1, true);
 			});
 		}catch(e) {
 			node.error(e);
 		}
 	}
-	red.nodes.registerType("oled", oled);
+	red.nodes.registerType("oled", Oled);
 }
