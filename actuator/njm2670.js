14b2311b72f0609143c8437b19d695a77ed9282e
KokiOta <ma20015@shibaura-it.ac.jp>
2021-03-26 23:07:57 +0900

    motor was implemented

--- actuator/njm2670.js
+++ actuator/njm2670.js
@@ -1,3 +1,9 @@
+const Gpio = require('pigpio').Gpio;
+
+var motor1 = null;
+var motor2 = null;
+var motorP = null;
+
 module.exports = function(red) {
 	function njm2670(conf) {
 		red.nodes.createNode(this, conf);
@@ -5,7 +11,19 @@ module.exports = function(red) {
 
 		try {
 			node.on('input', function(msg) {
-				node.send("Hello!");
+				if(msg.select=='A'){
+					motor1 = new Gpio(6, {mode: Gpio.OUTPUT});
+					motor2 = new Gpio(5, {mode: Gpio.OUTPUT});
+					motorP = new Gpio(12, {mode: Gpio.OUTPUT});
+				}else if(msg.select=='B'){
+					motor1 = new Gpio(16, {mode: Gpio.OUTPUT});
+					motor2 = new Gpio(26, {mode: Gpio.OUTPUT});
+					motorP = new Gpio(13, {mode: Gpio.OUTPUT});
+				}
+				motor1.digitalWrite(msg.mode1);
+				motor2.digitalWrite(msg.mode2);
+				motorP.pwmWrite(msg.dutyCycle);
+				node.send(msg);
 			});
 		}catch(e) {
 			node.error(e);
